236eaab6c214176efdcbe19eae53956902d89534
Nhat Quach <nhatqm@unitech.vn>
2024-12-16 11:36:14 +0700

    update: content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default async function Projects() {
         className=" animate-pulse absolute left-0 bottom-16 sm:bottom-5 flex rounded-r-full justify-center items-center gap-2 z-50 w-fit h-fit p-2 shadow-md  border-y border-r  border-black hover:bg-primary hover:text-white hover:animate-none "
       >
         <IconBrandGithub />
-        <span className="font-rubik text-2xl max-sm:text-xl">Github</span>
+        <span className="font-rubik text-lg max-sm:text-base">Github</span>
       </Link>
     </Container>
   );
--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -4,32 +4,50 @@ const listPictureTour = Array.from({ length: 14 }).map(
 export const products = [
   {
     href: "https://aceternity.com",
-    title: "Aceternity",
+    title: "Tour Experience Booking System",
     description:
       "A system that streamlines tour management and booking, making the process fast and convenient. It provides users with easy and intuitive access while ensuring transparent and secure storage of personal information through Blockchain technology.",
     thumbnail: listPictureTour[0],
     images: listPictureTour,
-    stack: ["Nextjs", "Tailwindcss"],
-    slug: "aceternity",
+    stack: [
+      "Nextjs",
+      "Tailwindcss",
+      "Redux",
+      "Tanstack Query",
+      "Typescript",
+      "Charka UI",
+      "Solona",
+    ],
+    slug: "tour-experience-booking-system",
     content: (
-      <div>
+      <>
+        <h1>Tour Experience Booking System</h1>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+          The tour experience booking system is developed to offer users
+          groundbreaking conveniences and create a refreshing experience. Users
+          have the ability to become trip organizers and create tours based on
+          their personal experiences. They can provide and manage tour
+          information, including destinations, activities, itineraries, pricing,
+          and more.
         </p>
         <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
-      </div>
+          For users looking to explore organized tours, they can search for and
+          book tours from the list of available options. The system will display
+          comprehensive information about these tours, including reviews from
+          other users.
+        </p>
+        <p>
+          Tour booking information will be verified and stored using Blockchain
+          technology. This ensures the safety and transparency of the
+          information, preventing issues related to forgery or tampering.
+          Transactions and related data will be recorded on the blockchain,
+          creating an immutable and tamper-proof system.
+        </p>
+        <p>
+          Additionally, the system is integrated with ChatGPT to assist users in
+          finding available tours within the system.
+        </p>
+      </>
     ),
   },
 ];
