ba40da01ea26c2926db2c7985718cda2affb54a0
Nhat Quach <nhatqm@unitech.vn>
2025-02-10 15:03:16 +0700

    update: add meta tags for SEO optimization in Projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Container } from "@/components/Container";
 import HeroTexts from "@/components/HeroTexts";
 import { TechStack } from "@/components/TechStack";
 import { Metadata } from "next";
+import Head from "next/head";
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -14,41 +15,43 @@ export const metadata: Metadata = {
 
 export default async function Projects() {
   return (
-    <Container className="bg-[radial-gradient(#00cfab_1px,transparent_1px)] [background-size:16px_16px] flex items-center justify-center min-h-screen">
-      <div className="flex flex-wrap break-words items-center pb-4 gap-14">
-        <FramerWrapper
-          className="w-auto flex flex-col justify-start gap-4"
-          y={0}
-          x={-100}
-        >
-          <HeroTexts />
-          <div className="h-fit w-full p-4 flex items-center justify-center flex-wrap gap-4">
-            {/* <SocialLinks /> */}
-            <TechStack />
-          </div>
-        </FramerWrapper>
-        {/* RIGHT SIDE image  */}
-        <FramerWrapper
-          y={0}
-          x={100}
-        >
-          {/* IMAGE  */}
-          <a href="https://app.daily.dev/nhat1711">
-            <Image
-              src="https://api.daily.dev/devcards/v2/CSF1vbHz88qerLPko9lfv.png?type=default&r=ysu"
-              alt="MinhNhat's Dev Card"
-              height={200}
-              width={200}
-              objectFit="cover"
-              loading="lazy"
-              rel="preload"
-              fetchPriority="high"
-            />
-          </a>
-        </FramerWrapper>
+    <>
+      <Head>
+        <meta name="robots" content="index, follow" />
+      </Head>
+      <Container className="bg-[radial-gradient(#00cfab_1px,transparent_1px)] [background-size:16px_16px] flex items-center justify-center min-h-screen">
+        <div className="flex flex-wrap break-words items-center pb-4 gap-14">
+          <FramerWrapper
+            className="w-auto flex flex-col justify-start gap-4"
+            y={0}
+            x={-100}
+          >
+            <HeroTexts />
+            <div className="h-fit w-full p-4 flex items-center justify-center flex-wrap gap-4">
+              {/* <SocialLinks /> */}
+              <TechStack />
+            </div>
+          </FramerWrapper>
+          {/* RIGHT SIDE image  */}
+          <FramerWrapper y={0} x={100}>
+            {/* IMAGE  */}
+            <a href="https://app.daily.dev/nhat1711">
+              <Image
+                src="https://api.daily.dev/devcards/v2/CSF1vbHz88qerLPko9lfv.png?type=default&r=ysu"
+                alt="MinhNhat's Dev Card"
+                height={200}
+                width={200}
+                objectFit="cover"
+                loading="lazy"
+                rel="preload"
+                fetchPriority="high"
+              />
+            </a>
+          </FramerWrapper>
 
-        {/* GITHUB BUTTON  */}
-      </div>
-    </Container>
+          {/* GITHUB BUTTON  */}
+        </div>
+      </Container>
+    </>
   );
 }
